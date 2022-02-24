c206411f02af5622c61b79d41a8ce5d246688ed1
nkita <nkitam7@gmail.com>
2022-02-24 20:46:59 +0900

    Modify: ALDS1_4_B Search2

--- ALDS1/ALDS1_4_B_Search2.php
+++ ALDS1/ALDS1_4_B_Search2.php
@@ -7,31 +7,48 @@ $n = (int)rtrim(fgets(STDIN));
 $S = explode(" ", rtrim(fgets(STDIN)));
 $q = (int)rtrim(fgets(STDIN));
 $T = explode(" ", rtrim(fgets(STDIN)));
-
 $count = 0;
-foreach ($T as $t) {
-    // print("\$t = $t \n");
-    $end = $n - 1;
-    $start = 0;
-    $add = floor(($end - $start) / 2);
-    while ($add > 0) {
-        $mod = ($end - $start) % 2;
-        $add = floor(($end - $start) / 2);
-        $index = $start + $add;
-        // printf("start-end=[$start-$end] [%s,..,%s] S[$index]=%s mod $mod \n", $S[$start], $S[$end], $S[$index]);
-        if ($S[$index] == $t) {
-            $count++;
-            break;
-        }
-        if ($S[$index] > $t) {
-            $end = $index;
+foreach ($T as $t) if (binarySearch($S, $t)) $count++;
+print("$count\n");
+
+// foreach ($T as $t) {
+//     // print("\$t = $t \n");
+//     $end = $n - 1;
+//     $start = 0;
+//     $add = floor(($end - $start) / 2);
+//     while ($add > 0) {
+//         $mod = ($end - $start) % 2;
+//         $add = floor(($end - $start) / 2);
+//         $index = $start + $add;
+//         if ($S[$index] == $t) {
+//             $count++;
+//             break;
+//         }
+//         if ($S[$index] > $t) {
+//             $end = $index;
+//         } else {
+//             $start = $index;
+//         }
+//         if ($add <= 0 && $mod == 1) {
+//             if ($S[$index + 1] == $t) $count++;
+//             break;
+//         }
+//     }
+// }
+
+function binarySearch($S, $key)
+{
+    $left = 0;
+    $right = count($S);
+    while ($left < $right) {
+        $mid = ($left + $right) / 2;
+        if ($S[$mid] == $key) {
+            return $mid;
+        } elseif ($key < $S[$mid]) {
+            $right = $mid;
         } else {
-            $start = $index;
-        }
-        if ($add <= 0 && $mod == 1) {
-            if ($S[$index + 1] == $t) $count++;
-            break;
+            $left = $mid + 1;
         }
     }
+    return false;
 }
-print("$count\n");
